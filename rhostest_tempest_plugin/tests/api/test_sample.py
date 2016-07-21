76d967bf2197acf1ad83adc110095b93a17991eb
Joe H. Rahme <jhakimra@redhat.com>
2016-07-21 13:53:51 +0200

    Includes license and fixes copyright headers
    A bunch of other changes in the comments too.

--- rhostest_tempest_plugin/tests/api/test_sample.py
+++ rhostest_tempest_plugin/tests/api/test_sample.py
@@ -1,3 +1,17 @@
+# Copyright 2016 Red Hat
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 from tempest import test
 
 from tempest.api.compute import base
@@ -10,16 +24,11 @@ CONF = config.CONF
 class SampleTest(base.BaseV2ComputeTest):
 
     @test.attr(type="smoke")
-    def test_success(self):
+    def test_neutron_is_enabled(self):
+        # This shows how to access tempest CONF variables
         self.assertTrue(CONF.service_available.neutron)
 
-    def test_fail(self):
-        self.assertFalse(CONF.service_available.neutron)
-
     def test_create_server_with_admin_password(self):
-        # If an admin password is provided on server creation, the server's
-        # root password should be set to that password.
+        # This test shows how to use the tempest clients
         server = self.create_test_server(adminPass='testpassword')
-
-        # Verify the password is set correctly in the response
         self.assertEqual('testpassword', server['adminPass'])

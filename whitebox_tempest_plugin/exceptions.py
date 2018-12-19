8f4d347736f7c40caba216220051827f12c61979
Matthew Booth <mbooth@redhat.com>
2018-12-19 11:56:13 -0500

    Fix typo in MissingHypervisorException
    Change-Id: Iffce9a25ad4c936e23242a5c090cc1b617fdc630

--- whitebox_tempest_plugin/exceptions.py
+++ whitebox_tempest_plugin/exceptions.py
@@ -17,4 +17,4 @@ from tempest.lib import exceptions
 
 
 class MissingHypervisorException(exceptions.TempestException):
-    message = "Unable to find IP in conf. Server: %(sever)s, host: %(host)s."
+    message = "Unable to find IP in conf. Server: %(server)s, host: %(host)s."

da806af8bb150e8d7b7a3769a66fbfa3563036aa
Artom Lifshitz <alifshit@redhat.com>
2021-08-19 18:53:25 -0400

    Simplify test_live_migration
    We don't actually need guest networking for any of our live migration
    tests. Remove the setup_credentials() method that sets it up.
    
    Change-Id: Ie28461a9ad64dd02ce86d23e0f4b7766a776be9a

--- whitebox_tempest_plugin/api/compute/test_live_migration.py
+++ whitebox_tempest_plugin/api/compute/test_live_migration.py
@@ -49,18 +49,6 @@ class LiveMigrationBase(base.BaseWhiteboxComputeTest):
             raise cls.skipException(
                 "Less than 2 compute nodes, skipping migration test.")
 
-    @classmethod
-    def setup_credentials(cls):
-        # These tests don't attempt any SSH validation nor do they use
-        # floating IPs on the instance, so all we need is a network and
-        # a subnet so the instance being migrated has a single port, but
-        # we need that to make sure we are properly updating the port
-        # host bindings during the live migration.
-        # TODO(mriedem): SSH validation before and after the instance is
-        # live migrated would be a nice test wrinkle addition.
-        cls.set_network_resources(network=True, subnet=True)
-        super(LiveMigrationBase, cls).setup_credentials()
-
 
 class LiveMigrationTest(LiveMigrationBase):
     # First support for block_migration='auto': since Mitaka (OSP9)
